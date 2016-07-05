e01c02f6da7d1e3d259a43bec68d68b2a0195ba7
Kacey Reidy <kacey.reidy@gmail.com>
2016-07-05 16:25:18 -0700

    Deleted cumin(), aggregate(), and aggregategraph(); Removed graphing from cume() and gee(); Editted docstrings for clarification

--- pdbsurvey/coordination.py
+++ pdbsurvey/coordination.py
@@ -44,174 +44,80 @@ def en(protein, ion, maxdistance=20, oxynotprotein=True):
     df = df.reset_index()[columns]
     return df
 
-def cume(df, yaxis='distance', maxdistance=20, binnumber=20, nummols=1, ax=None):
+def cume(files, maxdistance=20, binnumber=20, nummols=None):
     """Creates a cumulative histogram of distances of oxygen atoms from an ion.
     :Arguments:
-        *df*
-            `pandas.DataFrame` containing resids, resnames, and atom names
-            for each oxygen surrounding the ion
-        *yaxis*
-            collumn in df to be graphed on the y-axis; default = distance
+        *files*
+            list of locations of files containing distance dataframes
         *maxdistance*
             maximum distance of interest from the ion; default = 20
         *binnumber*
             number of desired bins for cumulative histogram; default = 20
-        *ax*
-            axis to plot on; default = None
         *nummols*
-            number of proteins/molecules contributing to df; default = 1
-
-    :Prints:
-        *cumulative histogram*
-            number of oxygens within a radius of the ion
-        *distances table*
-            atom numbers, resids, resnames, atom names, and distances from the ion
-            of each oxygen
+            number of ions/molecules serving as centers contributing to df; default = None
 
     :Returns:
-        *ax*
-            axis used for plotting
+        *m*
+            midpoints of bins
+        *cumulative*
+            cumulative histogram values
     """
-    if ax is None:
-        fig = plt.figure(figsize = (4,3))
-        ax = fig.add_subplot(1,1,1)
-    h, e = np.histogram(df[df[yaxis] < maxdistance][yaxis], bins=binnumber)
+    x = 0
 
+    for fil in files:
+        try:
+            f = pd.read_csv(fil, index_col=0)
+            dataframe = pd.concat([dataframe, f])
+            x += 1
+        except:
+            with open('failures.out', 'a') as f:
+                f.write(files[x] + '\n')
+
+    if nummols is None:
+        nummols = x
+
+    h, e = np.histogram(dataframe[dataframe['distance'] < maxdistance]['distance'], bins=binnumber)
     h = h / float(nummols)
     cumulative = np.cumsum(h)
     m = .5 * (e[:-1] + e[1:])
-    ax.plot(m, cumulative)
-    return ax
+    return m, cumulative
 
-def cumin(protein, ions, yaxis='distance', maxdistance=20, oxynotprotein=True,
-          binnumber=20, nummols=1, ax=None):
-    """Creates a cumulative histogram of distances of oxygen atoms from an ion.
-    :Arguments:
-        *protein*
-            protein Universe
-        *ions*
-            AtomGroup of ions
-        *yaxis*
-            collumn in df to be graphed on the y-axis; default = distance
-        *maxdistance*
-            maximum distance of interest from the ion; default = 20
-        *oxynotprotein*
-            boolean value of whether to include oxygens not in the protein; default = False
-        *binnumber*
-            number of desired bins for cumulative histogram; default = 20
-        *nummols*
-            number of proteins/molecules contributing to df; default = 1
-        *ax*
-            axis to plot on; default = None
-
-    :Prints:
-        *cumulative histogram*
-            number of oxygens within a radius of the ion
-        *distances table*
-            atom numbers, resids, resnames, atom names, and distances from the ion of each oxygen
-
-    :Returns:
-        *ax*
-            axis used for plotting
-    """
-    if ax is None:
-        fig = plt.figure(figsize = (4,3))
-        ax = fig.add_subplot(1,1,1)
-    for ion in ions:
-        cume(en(protein=protein, ion=ion, maxdistance=maxdistance, oxynotprotein=oxynotprotein), yaxis=yaxis, maxdistance=maxdistance, binnumber=binnumber, nummols=nummols, ax=ax)
-    return ax
-
-def gee(files, filename, binnumber=20, nummols=1):
+def gee(files, filename, binnumber=20, nummols=None):
     '''Produces a graph of density as a function of distance
     :Arguments:
         *files*
-            list of dataframe file locations
+            list of locations of files containing distance dataframes
         *filename*
             desired name of output file
         *binnumber*
             number of desired bins for cumulative histogram; default = 20
         *nummols*
-            number of proteins/molecules contributing to df; default = 1
+            number of ions/molecules serving as centers contributing to df; default = None
     :Returns:
-        *graph*
-            graph of g(r)
+        *m*
+            midpoints of bins
+        *density*
+            density histogram values
     '''
     dataframe = pd.DataFrame()
-    for x in range(len(files)):
+    x = 0
+
+    for fil in files:
         try:
-            f = pd.read_csv(files[x], index_col=0)
-            dataframe = pd.concat([f], names=['pdbids'])
+            f = pd.read_csv(fil, index_col=0)
+            dataframe = pd.concat([dataframe, f])
+            x += 1
         except:
             with open('failures.out', 'a') as f:
                 f.write(files[x] + '\n')
+
+    if nummols is None:
+        nummols = x
 
     h, e = np.histogram(dataframe['distance'], bins=binnumber)
     m = .5 * (e[:-1] + e[1:])
     V = 4 / 3 * np.pi * (e[1:] ** 3 - e[:-1] ** 3)
 
-    density = h / V
-    density = density / nummols
+    density = h / V / nummols
 
-    ax = plt.subplot(1,1,1)
-    ax.plot(m, density)
-    ax.figure.savefig(filename)
-
-    return ax
-
-def aggregate(pdbids, path, ionname, maxdistance=20, oxynotprotein=True):
-    """Aggregates dataframes into one dataframe
-    :Arguments:
-        *pdbids*
-            list of PDB codes corresponding to .pdb files to be aggregated
-            (note that .pdb is not to be included in pdbids) 
-        *path*
-            path to the file's directory
-        *ionname*
-            name of reference ion
-        *maxdistance*
-            maximum distance of interest from the ion; default = 20
-        *oxynotprotein*
-            boolean value of whether to include oxygens not in the protein; default = True
-    :Returns:
-        *dataframe*
-            aggregated `pandas.DataFrame` containing resids, resnames, and atom names
-            for each oxygen in the protein file
-    """
-    dataframe = []
-
-    for x in range(len(pdbids)):
-        try:
-            u = mda.Universe(os.path.join(path, pdbids[x]) + '.pdb', guess_bonds = False, permissive = False)
-            ions = u.select_atoms('not protein and name ' + ionname + '*')
-            for i, ion in enumerate(ions):
-                dataframe.append(en(u, ion, maxdistance = maxdistance, oxynotprotein = oxynotprotein))
-                proteinids.append(pdbids[x] + '_{}'.format(i))
-                return dataframe
-        except IOError:
-            continue
-    return dataframe, proteinids
-
-def aggregategraph(pdbids, path, ionname, yaxis='distance',
-                   binnumber=20, maxdistance=20, oxynotprotein=True):
-    """Produces an aggregated graph from multiple dataframes
-    :Arguments:
-        *pdbids*
-            list of PDB codes corresponding to .pdb files to be aggregated (note that .pdb is not to be included in pdbids)
-        *path*
-            path to the file's directory
-        *ionname*
-            name of reference ion
-        *binnumber*
-            number of desired bins for cumulative histogram; default = 20
-        *maxdistance*
-            maximum distance of interest from the ion; default = 20
-        *oxynotprotein*
-            boolean value of whether to include oxygens not in the protein; default = True
-    :Returns:
-        *graph*
-            aggregated graph
-    """
-    dataframe = aggregate(pdbids, path, ionname=ionname,
-                          maxdistance=maxdistance, oxynotprotein=oxynotprotein)
-    df = pd.concat(dataframe, keys=proteinids, names=['pdbids'])
-    df[yaxis].plot(kind='hist', bins=binnumber)
+    return m, density

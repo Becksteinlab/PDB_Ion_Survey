b58f38b77d55d005a7a0165ca2367c8897ef4ea2
Kacey Reidy <kacey.reidy@gmail.com>
2016-12-16 15:57:20 -0700

    Removed pdfs, notebooks, and now package is installable.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python
+"""Setuptools-based setup script for pdbionsurvey.
+
+For a basic installation just type the command::
+
+  python setup.py install
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='pdbionsurvey',
+      version='0.1.0-dev',
+      description='survey of ion coordination in the Protein Data Bank',
+      author='Kacey Clark',
+      author_email='kacey.reidy@gmail.com',
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      scripts=[],
+      license='GPLv3',
+      long_description=open('README.md').read(),
+      install_requires=['numpy', 'pandas', 'MDAnalysis', 'mdsynthesis', 'peakutils', 'matplotlib']
+      )

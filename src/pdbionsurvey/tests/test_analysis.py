fb66976a4a95296806c1bc2d53b854ca6abe62d2
Kacey Reidy <kacey.reidy@gmail.com>
2018-09-06 14:48:40 -0700

    Changed name of test to make it normal

--- src/pdbionsurvey/tests/test_analysis.py
+++ src/pdbionsurvey/tests/test_analysis.py
@@ -8,8 +8,8 @@ import pdbionsurvey.analysis as analysis
 def test_ligmaking(tmpdir):
     p = tmpdir.mkdir('sims/')
     path = str(p)
-    analysis.make_sims('1AK0', path)
-    sim = mds.Sim(path+'1AK0')
+    analysis.make_sims('1AK0', path=path)
+    sim = mds.Sim(os.path.join(path, '1AK0'))
     analysis.pdb2pqr(sim)
     assert(sim[sim.name+'.pqr'].exists)
     analysis.ligsolution(sim)

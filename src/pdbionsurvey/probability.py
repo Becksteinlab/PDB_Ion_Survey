e4017aa2764d1e84f7e5a0e5df80fc4fe504c3f8
Kacey Reidy <kacey.reidy@gmail.com>
2019-03-11 10:08:48 -0700

    Removed import of mdsynthesis

--- src/pdbionsurvey/probability.py
+++ src/pdbionsurvey/probability.py
@@ -8,7 +8,6 @@ Functions for analyzing coordination functions
 '''
 
 import MDAnalysis as mda
-import mdsynthesis as mds
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
